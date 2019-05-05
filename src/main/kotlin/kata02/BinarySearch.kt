8e4d24d14ea73417e47da6dfc0092e2849a0de7b
James Little <alphafoobar@users.noreply.github.com>
2019-05-05 22:49:23 +1200

    Simplified binary search algorithm

--- src/main/kotlin/kata02/BinarySearch.kt
+++ src/main/kotlin/kata02/BinarySearch.kt
@@ -6,16 +6,11 @@ fun chop(key: Int, a: IntArray): Int {
 
 fun chopper(key: Int, a: IntArray, start: Int, size: Int): Int {
     val remaining = size - start
-
     if (remaining == 0) {
         return -1
     }
-    if (remaining == 1) {
-        return foundIndex(key, a, start)
-    }
 
-    val mid = remaining / 2
-    val point = start + mid
+    val point = start + remaining / 2
     if (a[point] == key) {
         return point
     }
@@ -23,11 +18,4 @@ fun chopper(key: Int, a: IntArray, start: Int, size: Int): Int {
         return chopper(key, a, start, point)
     }
     return chopper(key, a, point + 1, size)
-}
-
-fun foundIndex(key: Int, a: IntArray, i: Int): Int {
-    if (a[i] == key) {
-        return i
-    }
-    return -1
 }
